2fe0f2df69dc8e8581652e73b5c90724ee625b67
Nagacharan <89054489+charan-happy@users.noreply.github.com>
2024-10-18 16:33:34 +0000

    update

--- main.tf
+++ main.tf
@@ -1,13 +1,62 @@
 provider "aws" {
   region = "us-east-1"
 }
-/*
-- AWS has datacenters across the globe and they are grouped into regions. An AWS region is a separate geographical area, such as us-east-1, us-east-2, ohio etc. and within each region, there are multiple isolated datacenters known as "Availability zones (AZ)" such as us-east-1a, us-east-2a 
-*/
+
+variable "server_port" {
+  description = "The port the server will use for HTTP Requests"
+  type        = number
+  default     = 8080
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = var.server_port
+    to_port     = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_launch_template" "example" {
+  name_prefix   = "example-"
+  image_id      = "ami-0866a3c8686eaeeba" 
+  instance_type = "t2.micro"              
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      volume_size = 8
+      volume_type = "gp2"
+    }
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.instance.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "terraform-example-instance"
+    }
+  }
+
+  user_data = base64encode(<<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
+            )
+}
 
 resource "aws_instance" "example" {
-  ami = "ami-0866a3c8686eaeeba"
-  instance_type = "t2.micro"
+  ami                   = "ami-0866a3c8686eaeeba"
+  instance_type         = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
@@ -19,57 +68,39 @@ resource "aws_instance" "example" {
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p ${var.server_port} &
               EOF
+
   user_data_replace_on_change = true
 }
 
-resource "aws_security_group" "instance" {
-  name = "terraform-example-instance"
-
-  ingress {
-    from_port = var.server_port
-    to_port = var.server_port
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-variable "server_port" {
-  description = "The port the server will use for HTTP Request"
-  type = number
-  default = 8080
-}
-
-//autoscaling group
-resource "aws_launch_configuration" "example" {
-  image_id = "ami-0866a3c8686eaeeba"
-  instance_type = "t2.micro"
-  security_groups = [aws_security_group.instance.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p ${var.server_port} &
-              EOF
-  # required when using a launch configuration with an auto scaling group 
-  
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_autoscaling_group" "example" {
-  launch_configuration = aws_launch_configuration.example.name 
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
 
-  min_size = 2
-  max_size = 10
+  vpc_zone_identifier = data.aws_subnets.default.ids
+  min_size            = 2
+  max_size            = 10
 
   tag {
-    key = "Name"
-    value = "terraform asg-example"
+    key                 = "Name"
+    value               = "terraform asg-example"
     propagate_at_launch = true
   }
 }
+
 output "public_ip" {
-  value = aws_instance.example.public_ip
-  description = "the public IP address of the web server"
+  value       = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
