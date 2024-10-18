889bc7ee12db05f24663cfdfa43ecf9864902978
Nagacharan <89054489+charan-happy@users.noreply.github.com>
2024-10-18 17:43:13 +0000

    final destroy of chapter 2 practice

--- alb.tf
+++ alb.tf
@@ -1,7 +1,7 @@
 resource "aws_lb" "example" {
   name = "terraform-asg-example"
   load_balancer_type = "application"
-  subnets = data.aws_subnets.default.id
+  subnets = data.aws_subnets.default.ids
   security_groups = [aws_security_group.alb.id]
 }
 
